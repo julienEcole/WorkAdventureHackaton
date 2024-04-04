2af1934bc0f6f608c67f5f9bd5f5b4333f812d99
julien <julien.riviere94120@gmail.com>
2024-04-04 13:11:57 +0200

    feat : nouvelle map commencé a être modifié

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {Cron, parseCronExpression, TimerBasedCronScheduler as scheduler} from "cron-schedule";
+
 
 console.log('Script started successfully');
 
@@ -19,6 +21,28 @@ WA.onInit().then(() => {
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
+    console.log('Scripting API ready');
+
+    // At 19:00, hide the laptops
+    const cronStartNight : Cron = parseCronExpression('0 19 * * *');
+    scheduler.setInterval(cronStartNight, () => {
+        WA.room.hideLayer("above/laptops");
+    });
+
+    // At 7:00, show the laptops
+    const cronStartDay : Cron = parseCronExpression('0 7 * * *');
+    scheduler.setInterval(cronStartDay, () => {
+        WA.room.showLayer("above/laptops");
+    });
+
+    // If the player enters the room between 19:00 and 7:00, hide the laptops
+    const now : Date = new Date();
+    const startNight : Date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 0, 0);
+    const startDay : Date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 0, 0);
+    if (now > startNight || now < startDay) {
+        WA.room.hideLayer("above/laptops");
+    }
+
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
@@ -26,7 +50,7 @@ WA.onInit().then(() => {
 
 }).catch(e => console.error(e));
 
-function closePopup(){
+function closePopup() : void{
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
