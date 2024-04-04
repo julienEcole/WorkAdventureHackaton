53c4e772abcdfc250d5b2a34432910df0ab9ca9c
makhlkh2 <khalil.makhloufi@univ-rouen.fr>
2024-04-04 14:56:34 +0200

    created lobby, player status (ready or not ) and the possibility to start a game

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,61 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+import { RemotePlayerInterface } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { isMapIterator } from "util/types";
+//import { parseCronExpression } from "cron-schedule";
 
-console.log('Script started successfully');
+
 
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    WA.players.configureTracking();
+    startGame()
+    // placer les joueurs en dehors du carré vert
+    WA.room.area.onEnter('greenZone').subscribe(()=>{
+        WA.player.state.isReady = true;
+        if(checkPlayers()){
+            console.log("lancement de la popup")
+            const buttonDescriptor = {
+            id: "startButton",
+            label: "Start",
+            callback: () => {
+                console.log('fermer')
+                closePopup();
+                startCountDown();
+            }
+        };
+        currentPopup = WA.ui.openPopup("countDown","",[buttonDescriptor])
+        }
+    }
+    )
+    WA.room.area.onLeave('greenZone').subscribe(()=>{
+        WA.player.state.isReady = false;
+        console.log('is ready player ' + WA.player.state.isReady)
+        console.log(checkPlayers())
+        if(checkPlayers()){
+            console.log("Tout les jouers sont prets")
+        }else{
+            console.log("Cerrtains  jouers sont pas prets")
+        }
+    });
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
+
+
+function startGame(){
+    WA.nav.goToRoom('lobby.tmj')
+}
 
 function closePopup(){
     if (currentPopup !== undefined) {
@@ -33,4 +64,34 @@ function closePopup(){
     }
 }
 
+function checkPlayers(){
+    const players = Array.from(WA.players.list());
+    if(WA.player.state.isReady == false||players.length == 0){
+        return false;
+    }
+    for (const player of players) {
+        if(player.state.isReady == false ){
+            return false;
+        }
+    }
+    return true;
+        
+}
+
+function startCountDown() {
+    var secondsLeft = 10;
+    const interval = setInterval(() => {
+        closePopup();
+        console.log('test')
+        currentPopup = WA.ui.openPopup("clockPopup", secondsLeft + " seconds", []);
+        secondsLeft--;
+        if (secondsLeft < 0) {
+            clearInterval(interval);
+            closePopup();
+        }
+    }, 1000);
+}
+
+
 export {};
+
