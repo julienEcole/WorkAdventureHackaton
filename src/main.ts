e688c5977c798fd1d5deae4bc9349f25016424be
makhlkh2 <khalil.makhloufi@univ-rouen.fr>
2024-04-04 16:47:42 +0200

    lobby finished

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,22 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-//import { parseCronExpression } from "cron-schedule";
+
 
 
 
 let currentPopup: any = undefined;
+let interval:any = undefined;
 
-// Waiting for the API to be ready
 WA.onInit().then(() => {
     WA.players.configureTracking();
-    startGame()
-    // placer les joueurs en dehors du carré vert
-    WA.room.area.onEnter('greenZone').subscribe(()=>{
-        WA.player.state.isReady = true;
-        if(checkPlayers()){
-            console.log("lancement de la popup")
-            const buttonDescriptor = {
-            id: "startButton",
-            label: "Start",
-            callback: () => {
-                console.log('fermer')
-                closePopup();
-                startCountDown();
-            }
-        };
-        currentPopup = WA.ui.openPopup("countDown","",[buttonDescriptor])
-        }
-    }
-    )
-    WA.room.area.onLeave('greenZone').subscribe(()=>{
-        WA.player.state.isReady = false;
-        console.log('is ready player ' + WA.player.state.isReady)
-        console.log(checkPlayers())
-        if(checkPlayers()){
-            console.log("Tout les jouers sont prets")
-        }else{
-            console.log("Cerrtains  jouers sont pas prets")
-        }
-    });
+    
+    //    startGame();
+        //gameStarted = true;
+    
 
-
-
+    listenOnGreenZone();
+    listenOnPlayers();
 
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
@@ -51,9 +26,9 @@ WA.onInit().then(() => {
 
 
 
-function startGame(){
+/*function startGame(){
     WA.nav.goToRoom('lobby.tmj')
-}
+}*/
 
 function closePopup(){
     if (currentPopup !== undefined) {
@@ -78,18 +53,61 @@ function checkPlayers(){
 
 function startCountDown() {
     var secondsLeft = 10;
-    const interval = setInterval(() => {
+    interval = setInterval(() => {
         closePopup();
         console.log('test')
-        currentPopup = WA.ui.openPopup("clockPopup", secondsLeft + " seconds", []);
+        currentPopup = WA.ui.openPopup("countDown", secondsLeft + " seconds", []);
         secondsLeft--;
         if (secondsLeft < 0) {
             clearInterval(interval);
             closePopup();
+            WA.event.broadcast("started",null)
         }
     }, 1000);
 }
 
+
+
+function listenOnPlayers(){
+    WA.event.on("started").subscribe(() => {
+        WA.nav.goToRoom('map.tmj');
+    });
+}
+
+function listenOnGreenZone(){
+    WA.room.area.onEnter('greenZone').subscribe(()=>{
+        WA.player.state.isReady = true;
+        if(checkPlayers()){
+            console.log("lancement de la popup")
+            closePopup();
+            const buttonDescriptor = {
+            id: "startButton",
+            label: "Start",
+            callback: () => {
+                console.log('fermer')
+                closePopup();
+                startCountDown();
+            }
+        };
+        currentPopup = WA.ui.openPopup("countDown","Lancer la partie ?",[buttonDescriptor])
+        }
+    }
+
+
+    )
+    WA.room.area.onLeave('greenZone').subscribe(()=>{
+        WA.player.state.isReady = false;
+        console.log('is ready player ' + WA.player.state.isReady)
+        console.log(checkPlayers())
+        if(checkPlayers()){
+            console.log("Tout les jouers sont prets")
+        }else{
+            closePopup();
+            clearInterval(interval);
+        }
+    });
+
+}
 
 export {};
 
