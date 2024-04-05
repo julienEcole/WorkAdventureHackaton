4f4a189395c1853c2bc5b3b365b4404ec4943b3d
julien <julien.riviere94120@gmail.com>
2024-04-05 09:14:43 +0200

    fix : affichage des taches bon, fix : tache etc non attribuable désormais, fix : correction du multi import de player

--- src/class/Taches.ts
+++ src/class/Taches.ts
@@ -3,6 +3,6 @@ export enum Taches {
     PUT_VERSION_ONLINE = "uploader son travail",
     THE_SPAGETTI_CABLE_MANAGEMENT = "trouver le bon cable",
     BADGE_SCAN = "scanner son badge",
-    ROCK_PAPER_CISOR_CHALLENGE = "un défi que vous ne pouvez refuser",
-    ETC = "etc"
+    ROCK_PAPER_CISOR_CHALLENGE = "un défi que vous ne pouvez refuser"
+    //ETC = "etc"
 }
